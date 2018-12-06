62cde94d79906e769bbb2c3196374cfe0d8d240c
ggomi <dhkdn4042@naver.com>
2018-12-06 14:50:24 +0900

    모바일화면 대응 및 리스트 출력화면 수정, 타이틀바 공간 확보

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import Webtoon from './Webtoon'
-import Header from './Layout/Header'
+import Webtoon from './Webtoon';
+// import Button from './Layout/TabBtn';
+import Header from './Layout/Header';
 class App extends Component {
   constructor(){
     super()
@@ -9,36 +10,52 @@ class App extends Component {
     }
   }
   
-  componentDidMount(){
-    fetch("/list").then(res=> res.json())
-    .then(data=> {
-      this.setState({
-         data : data
-      })
-    })
+  componentDidMount() {
+    document.title = "MyToon | JeongminOh";
+
+    fetch("/list").then(res => res.json())
+      .then(data => {
+        this.setState({
+          data: data
+        });
+      });
   }
 
   render() {
-    const {data} = this.state;
-
-    if(!data){
-      return <h2>로딩 중...</h2>
+    const { data } = this.state;
+    let date = new Date();
+    date = date.getDay();
+    
+    if(date === 0) {
+      date = 6;
     } else {
-      
-      const weekArr = ["MON","TUE","WED","THU","FRI","SAT","SUN"];
+      date = date - 1;
+    }
+
+    if (!data) {
+      return <h2>로딩중........</h2>
+    } else {
+      const weekArr = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
       
       return (
-        <div className="webtoon-list">
+        <div>
           <Header></Header>
-          {weekArr.map(function (object, i) {
-            const column = data[object].map((value, index) => <Webtoon name={value.toon_name} day={value.serialize_day} img={value.toon_imgsrc} href={value.toon_href} key={index} />);
-            return (
-              <div className="daliy-list">
-                <h3>{weekArr[i]}</h3>
-                {column}
-              </div>
-            )
-          })}
+          <div className="webtoon-list">
+            {weekArr.map(function (object, i) {
+              const column = data[object].map((value, index) => <Webtoon name={value.toon_name} day={value.serialize_day} img={value.toon_imgsrc} href={value.toon_href} key={index} />);
+              let className = "daliy-list side";
+              if(i === date) {
+                className = "daliy-list side today";
+              } 
+
+              return (
+                <div className={className} key={i}>
+                  <div className="date-header">{weekArr[i]}</div>
+                  {column}
+                </div>
+              )
+            })}
+          </div>
         </div>
       );
     }
--- Layout/Header.js
+++ Layout/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    search(){
+        
+    };
     render() {
         return (
-            <div>
-                
+            <div className="title-bar">
+                <input id="searchBox"type="text"/>
+                <button className="btn search-btn" onClick={this.search()}><img src={require("../commons/images/search-btn.png")} alt="search"></img></button>
             </div>
         );
     }
