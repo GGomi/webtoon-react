a86699f63ac2ee045899b5d9f13afb663208e8c5
Mark <dhkdn4042@naver.com>
2019-10-29 11:33:19 +0900

    Daum Webtoon pc / mobile link 설정

--- src/Webtoon.js
+++ src/Webtoon.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import './App.css'
 class Webtoon extends Component {
-    
+    isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
     render() {
         const {name, img, href, provider} = this.props;
-        const prefixHref = provider === "NAVER" ? "https://comic.naver.com" : "http://webtoon.daum.net";
+        
+        let prefixHref;
+        if (provider === "NAVER") {
+            prefixHref = "https://comic.naver.com";
+        } else if(provider === "DAUM") {
+            prefixHref = this.isMobile() ? "http://m.webtoon.daum.net/m" : "http://webtoon.daum.net";
+        }
+
         const link =  prefixHref + href;
         
         return (
